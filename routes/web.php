3093518192768d53c77ac8c06b387fef6b7fef8e
unknown <milosbviser@gmail.com>
2024-07-18 23:30:50 +0200

    New pages created. Added routes for the new pages.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/what-we-do', function () {
+    return view('what-we-do');
+});
+
+Route::get('/who-we-are', function () {
+    return view('who-we-are');
+});
